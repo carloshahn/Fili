2c200e3d370829249dbf6583467ce6efdc5dbfcc
student <student@student03.local>
2018-09-24 11:32:57 -0300

    gambiarra

--- js/creative.js
+++ js/creative.js
@@ -167,5 +167,5 @@ function showSlides() {
     slideIndex++;
     if (slideIndex > slides.length) {slideIndex = 1} 
     slides[slideIndex-1].style.display = "block"; 
-    setTimeout(showSlides, 4000); // Change image every 4 seconds
+    setTimeout(showSlides, 3000); // Change image every 4 seconds
 }
